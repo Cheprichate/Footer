c488c053113e74d12b1ddab42543801f697c4531
Cheprichate <petrovich15071998@mail.ru>
2023-10-26 15:23:22 +0400

    Добавьте файлы проекта.

---
+++ Footer.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+int width,mark,a;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите общую длину колонтитула:";
+    std::cin >> width;
+    std::cout << "Кол-во восклицательных знаков: ";
+    std::cin >> mark;
+
+    if (width >= mark) {
+        for (int a = 0; a < (width - mark) / 2; a++) {
+            std::cout << " - ";
+        }
+        for (int a = 0; a < mark; a++) {
+            std::cout << " ! ";
+        }
+        for (int a = 0; a < (width - mark) / 2 + (width % 2 != 0); a++) {
+            std::cout << " - ";
+        }
+    }
+}
